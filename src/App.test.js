66bb1496527ddefea84318da695b5650f0d38437
Shaun <milleriishaun@gmail.com>
2020-01-11 23:26:25 -0800

    chore: Add exclamation point
    # Details: add !
    # Issue no: 1
    # Feature no: 1
    #

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import App from "./App";
 import React from "react";
 import { render } from "@testing-library/react";
 
+// Test 1
 test("renders learn react link", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
